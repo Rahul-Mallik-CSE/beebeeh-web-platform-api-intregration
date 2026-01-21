d7d90ce95daa8e1770b002125c6672b13b7fbdb8
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-01-21 15:17:52 +0600

    Refactor input ref assignment in VerifyEmailForm for improved readability

--- src/components/AuthComponents/VerifyEmailForm.tsx
+++ src/components/AuthComponents/VerifyEmailForm.tsx
@@ -130,7 +130,9 @@ const VerifyEmailForm = () => {
           {otp.map((digit, index) => (
             <input
               key={index}
-              ref={(el) => (inputRefs.current[index] = el)}
+              ref={(el) => {
+                inputRefs.current[index] = el;
+              }}
               type="text"
               maxLength={1}
               value={digit}
